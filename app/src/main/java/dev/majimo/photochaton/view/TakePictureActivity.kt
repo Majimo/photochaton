9e8255793d0a8a8f12f773ad1f32a2bf9ca20cd9
Pierre FERVEL <pierre.fervel@gmail.com>
2018-11-26 16:12:51 +0100

    Camera Preview prototype

--- app/src/main/java/dev/majimo/photochaton/view/TakePictureActivity.kt
+++ app/src/main/java/dev/majimo/photochaton/view/TakePictureActivity.kt
@@ -3,11 +3,18 @@ package dev.majimo.photochaton.view
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import dev.majimo.photochaton.R
+import dev.majimo.photochaton.view.preview.CameraPreview
+
 
 class TakePictureActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_take_picture)
+
+        if (null == savedInstanceState) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, CameraPreview.newInstance())
+                    .commit()
+        }
     }
 }

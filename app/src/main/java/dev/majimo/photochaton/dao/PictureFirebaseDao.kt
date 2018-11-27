88088766450d80f0acf7da3d2657b24008ed2324
Natty <felix.soignon@gmail.com>
2018-11-27 11:05:54 +0100

    /AddSQL: Auto stash before merge of "feature/AddSQL" and "feature/file_saver"

--- app/src/main/java/dev/majimo/photochaton/dao/PictureFirebaseDao.kt
+++ app/src/main/java/dev/majimo/photochaton/dao/PictureFirebaseDao.kt
@@ -1,10 +1,37 @@
 package dev.majimo.photochaton.dao
 
+import android.net.Uri
+import com.google.firebase.storage.FirebaseStorage
 import dev.majimo.photochaton.model.Picture
+import com.google.firebase.storage.StorageReference
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.firebase.storage.UploadTask
+import com.google.android.gms.tasks.OnSuccessListener
+import dev.majimo.photochaton.service.FileService
+import dev.majimo.photochaton.service.IFileService
+
 
 class PictureFirebaseDao : IPictureFirebaseDao {
+
+    private var mStorageRef: StorageReference? = null
+
+    init {
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+    }
+
     override fun insert(picture: Picture) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val file = Uri.fromFile(FileService().createImageFile("Ceci/est/mon/chemin"))
+        val riversRef = mStorageRef?.child("images/rivers.jpg")
+
+        riversRef?.putFile(file)
+                ?.addOnSuccessListener(OnSuccessListener<UploadTask.TaskSnapshot> { taskSnapshot ->
+                    // Get a URL to the uploaded content
+                    val downloadUrl = taskSnapshot.downloadUrl
+                })
+                ?.addOnFailureListener(OnFailureListener {
+                    // Handle unsuccessful uploads
+                    // ...
+                })
     }
 
     override fun get(id: Int): Picture {

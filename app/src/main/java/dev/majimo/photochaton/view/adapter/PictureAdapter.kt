01ac3436cfe66d05bf309c9ea70112905a4095bd
Pierre FERVEL <pierre.fervel@gmail.com>
2018-11-28 09:48:54 +0100

    feature(popup) : Add Popup onClick picture from Adapter

--- app/src/main/java/dev/majimo/photochaton/view/adapter/PictureAdapter.kt
+++ app/src/main/java/dev/majimo/photochaton/view/adapter/PictureAdapter.kt
@@ -11,10 +11,18 @@ import android.widget.TextView
 import dev.majimo.photochaton.R
 import dev.majimo.photochaton.model.Picture
 import java.util.ArrayList
+import android.content.Context.LAYOUT_INFLATER_SERVICE
+import android.util.Log
+import android.widget.Button
+import android.widget.PopupWindow
+
+
+
+
 
 class PictureAdapter (val context: Context) : RecyclerView.Adapter<PictureAdapter.PictureViewHolder>() {
 
-     var items : List<Picture> = ArrayList()
+    var items : List<Picture> = ArrayList()
 
     fun setPictures(pictures : List<Picture>?){
         if (pictures == null){
@@ -30,14 +38,35 @@ class PictureAdapter (val context: Context) : RecyclerView.Adapter<PictureAdapte
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PictureViewHolder {
-        return PictureViewHolder(LayoutInflater.from(context).inflate(R.layout.line_photo, parent, false))
+        return PictureViewHolder(LayoutInflater.from(context).inflate(R.layout.line_photo, parent, false), context)
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
 
-    class PictureViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+    class PictureViewHolder (view: View, val context: Context) : RecyclerView.ViewHolder(view), View.OnClickListener {
         val iv_picture = view.findViewById<ImageView>(R.id.iv_picture)
+
+        init {
+            view.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val layoutInflater = context
+                    .getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val popupView = layoutInflater.inflate(R.layout.popup_picture, null)
+            val popupWindow = PopupWindow(
+                    popupView,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT)
+
+            val btnDismiss = popupView.findViewById<Button>(R.id.bnt_popup_dismiss)
+            btnDismiss.setOnClickListener(View.OnClickListener {
+                popupWindow.dismiss()
+            })
+
+            popupWindow.showAsDropDown(v, 20, 20)
+        }
     }
 }

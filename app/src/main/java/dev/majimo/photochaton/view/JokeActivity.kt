4c8ad953424d082c2f3d119c44e9ad1a98602d82
MamzellClairounette <blue-sky-cr@hotmail.fr>
2018-11-28 11:20:56 +0100

    Fix api slip.com

---
+++ app/src/main/java/dev/majimo/photochaton/view/JokeActivity.kt
@@ -0,0 +1,11 @@
+package dev.majimo.photochaton.view
+
+import android.support.v7.app.AppCompatActivity
+
+class JokeActivity: AppCompatActivity() {
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
--- app/src/main/java/dev/majimo/photochaton/view/MainActivity.kt
+++ app/src/main/java/dev/majimo/photochaton/view/MainActivity.kt
@@ -2,12 +2,23 @@ package dev.majimo.photochaton.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import com.facebook.stetho.Stetho
 import dev.majimo.photochaton.R
+import dev.majimo.photochaton.model.Slip
+import dev.majimo.photochaton.service.JokeService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : MenuActivity() {
     private var button: Button? = null
+
+    private var tvJoke: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,11 +26,49 @@ class MainActivity : MenuActivity() {
 
         Stetho.initializeWithDefaults(this)
 
+        tvJoke = findViewById(R.id.tv_joke)
+
         button = findViewById(R.id.btn_launcher) as Button
 
         button!!.setOnClickListener {
             val intent = Intent(this, TakePictureActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getJoke()
+    }
+
+    private fun getJoke() {
+        val url = "https://api.adviceslip.com/"
+
+        Log.wtf("XXX", "Es ce que tu passe dedans ?")
+
+        val retrofit = Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+
+        val service = retrofit.create(JokeService::class.java)
+
+        service.listJoke().enqueue(object : Callback<Slip> {
+
+            override fun onResponse(call: Call<Slip>, response: Response<Slip>) {
+                val joke = response.body()
+
+                Log.wtf("XXX", "onResponse")
+
+                print(" one course : ${joke?.slip?.slip_id} : ${joke?.slip?.advice} ")
+                tvJoke?.text = joke?.slip?.advice ?: ""
+            }
+
+            override fun onFailure(call: Call<Slip>, t: Throwable) {
+                //Log.wtf("XXX", "T'es là onFailure" + t.message)
+                //error("KO : $t")
+                t.printStackTrace()
+            }
+        })
     }
 }
